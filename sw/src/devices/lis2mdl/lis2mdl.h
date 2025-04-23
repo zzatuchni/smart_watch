ecf1e726c7a2cb722045c5059378ddc78fd73f9c
Zachary Zatuchni <zzatuchni@outlook.com>
2025-04-23 01:06:33 -0400

    Starting this again

--- sw/src/devices/lis2mdl/lis2mdl.h
+++ sw/src/devices/lis2mdl/lis2mdl.h
@@ -1,10 +1,10 @@
 #ifndef LIS2MDL_H
 #define LIS2MDL_H
 
-#include "../stm32l47x/basic.h"
-#include "../stm32l47x/uart.h"
-#include "../stm32l47x/gpio.h"
-#include "../stm32l47x/i2c.h"
+#include "basic.h"
+#include "uart.h"
+#include "gpio.h"
+#include "i2c.h"
 
 #define LIS2MDL_I2C_ADDRESS 0x1E
 
@@ -25,17 +25,18 @@
 
 #define LIS2MDL_OFFSETX_L_REG_ADDRESS 0x45
 #define LIS2MDL_OFFSETX_H_REG_ADDRESS 0x46
-#define LIS2MDL_OFFSETX_VALUE (int16_t)(0)
+#define LIS2MDL_OFFSETX_VALUE (int16_t)(-455)
 
 #define LIS2MDL_OFFSETY_L_REG_ADDRESS 0x47
 #define LIS2MDL_OFFSETY_H_REG_ADDRESS 0x48
-#define LIS2MDL_OFFSETY_VALUE (int16_t)(0)
+#define LIS2MDL_OFFSETY_VALUE (int16_t)(-500)
 
 #define LIS2MDL_OFFSETZ_L_REG_ADDRESS 0x49
 #define LIS2MDL_OFFSETZ_H_REG_ADDRESS 0x4A
 #define LIS2MDL_OFFSETZ_VALUE (int16_t)(0)
 
-#define LIS2MDL_CFG_REG_A_STARTUP_VALUE 0x80
+#define LIS2MDL_CFG_REG_A_STARTUP_VALUE 0x8C
+#define LIS2MDL_CFG_REG_B_STARTUP_VALUE 0x01
 #define LIS2MDL_CFG_REG_C_STARTUP_VALUE 0x01
 
 typedef struct {
@@ -47,7 +48,5 @@ typedef struct {
 Result lis2mdl_init();
 
 Result lis2mdl_get_raw_data(Magnetometer_Raw_Data *mgnmtr_data);
-
-int16_t lis2mdl_get_heading_degrees(int16_t x, int16_t y);
 
 #endif
